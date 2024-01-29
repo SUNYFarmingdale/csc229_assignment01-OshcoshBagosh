f19afb942ad88a3ab0764adb7deb8d1f44490806
OshcoshBagosh <oscargabrguerrero@gmail.com>
2024-01-29 11:41:05 -0500

    layout

---
+++ src/main/java/week_01.java
@@ -0,0 +1,15 @@
+class week_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+class StudentId {
+    private int id;
+    private String name;
+    private int code;
+
+    public StudentId(){
+
+    }
+}
